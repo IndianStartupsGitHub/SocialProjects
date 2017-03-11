989e7861b5a9dd27a06167be1fdf020c1e321e57
Admin <admin@social1>
2017-03-11 07:06:41 -0800

    Added new social project to git

---
+++ Social1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Social1.MvcApplication" Language="C#" %>
---
+++ Social1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Social1.Startup))]
+namespace Social1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
